dfa093770cb8fc369a784c3ab3a7900f2f8e9bf0
Adam Cameron <cameron.adam+github@gmail.com>
2025-09-05 12:28:30 +0100

    IV-8: vitest installed and operational

---
+++ src/lt-8/slow.ts
@@ -0,0 +1,9 @@
+async function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function mySlowFunction(): Promise<void> {
+    console.log("Starting slow function...");
+    await sleep(2000);
+    console.log("Slow function finished.");
+}
