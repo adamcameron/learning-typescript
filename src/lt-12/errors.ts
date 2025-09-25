36335a28d8e53e720a8b939a6bbe7f6f50743317
Adam Cameron <cameron.adam+github@gmail.com>
2025-09-25 15:07:56 +0100

    LT-12: errors

---
+++ src/lt-12/errors.ts
@@ -0,0 +1,38 @@
+export function throwsError(message: string) {
+  throw Error(message)
+}
+
+export class MyError extends Error {}
+
+export function throwsMyError(message: string) {
+  throw new MyError(message)
+}
+
+export class MyDetailedError extends Error {
+  detail: string
+  constructor(message: string, detail: string) {
+    super(message)
+    this.detail = detail
+  }
+}
+
+export function throwsMyDetailedError(message: string, detail: string) {
+  throw new MyDetailedError(message, detail)
+}
+
+export function throwsString(s: string) {
+  throw s // eslint-disable-line @typescript-eslint/only-throw-error
+}
+
+export function throwsSyntaxError(s: string) {
+  throw new SyntaxError(s)
+}
+
+export function throwsErrorWithCause(
+  causeMessage: string,
+  errorMessage: string
+) {
+  const cause: Error = new Error(causeMessage)
+
+  throw new Error(errorMessage, { cause: cause })
+}
