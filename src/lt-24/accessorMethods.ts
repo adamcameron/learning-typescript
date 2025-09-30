bedd6af0c00ac8ba580d526556d1e3b06199ea91
Adam Cameron <cameron.adam+github@gmail.com>
2025-09-30 13:04:57 +0100

    LT-24: implementation improvements from GitHub copilot

--- src/lt-24/accessorMethods.ts
+++ src/lt-24/accessorMethods.ts
@@ -48,7 +48,9 @@ export class Account {
     const encoder = new TextEncoder()
     const data = encoder.encode(password)
     const hash = await crypto.subtle.digest('SHA-256', data)
-    const hashAsString = Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join('')
+    const hashAsString = Array.from(new Uint8Array(hash))
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('')
 
     return hashAsString
   }
