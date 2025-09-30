f2aed0632e601824d461bd9dfc7078962ecacee9
Adam Cameron <cameron.adam+github@gmail.com>
2025-09-30 15:36:47 +0100

    LT-30: bang operator usage

---
+++ src/lt-30/bangUsage.ts
@@ -0,0 +1,39 @@
+export interface LooseObject {
+  [key: string]: any // eslint-disable-line @typescript-eslint/no-explicit-any
+}
+
+export type NullableString = string | null
+export type NullableLooseObject = LooseObject | null
+
+export class DefiniteAssignmentAssertionExample {
+  private s!: string // without ! we get "Property 's' has no initializer and is not definitely assigned in the constructor"
+
+  setStringValue(v: string) {
+    this.s = v
+  }
+
+  double() {
+    return this.s + this.s
+  }
+}
+
+export class NonNullAssertionExample {
+  private s: string | null = null
+  private a: string[] = []
+
+  setStringValue(v: string) {
+    this.s = v
+  }
+
+  double() {
+    return this.s! + this.s // without !, we get " Object is possibly 'null'"
+  }
+
+  setArrayValue(a: string[]) {
+    this.a = a
+  }
+
+  findInArray(s: string): string {
+    return this.a.find((el) => el === s)! // without the ! we'd get "Type 'string | undefined' is not assignable to type 'string'"
+  }
+}
---
+++ tests/lt-30/bangUsage.test.ts
@@ -0,0 +1,58 @@
+import {
+  DefiniteAssignmentAssertionExample as DAAE,
+  NonNullAssertionExample as NNAE,
+} from '@/lt-30/bangUsage'
+import { describe, it, expect } from 'vitest'
+
+describe('Testing definite assignment assertion', () => {
+  it('says that an assignment has indeed taken place', () => {
+    const sut: DAAE = new DAAE()
+
+    sut.setStringValue('NOT_NULL')
+
+    expect(sut.double()).toEqual('NOT_NULLNOT_NULL')
+  })
+
+  it('will break at runtime if an assignment has not indeed taken place', () => {
+    const sut: DAAE = new DAAE()
+
+    expect(sut.double()).toBeNaN()
+  })
+
+  describe('Testing non-null assertion operator', () => {
+    it('says that the variable definitely is not null', () => {
+      const sut: NNAE = new NNAE()
+
+      sut.setStringValue('NOT_NULL')
+
+      expect(sut.double()).toEqual('NOT_NULLNOT_NULL')
+    })
+
+    it('will break at runtime if an assignment has not indeed taken place', () => {
+      const sut: NNAE = new NNAE()
+
+      expect(sut.double()).toEqual(0) // FML: null + null = 0
+    })
+
+    it('says that the array element was definitely found', () => {
+      const sut: NNAE = new NNAE()
+
+      sut.setArrayValue(['tahi', 'rua', 'toru', 'wha'])
+
+      expect(sut.findInArray('toru')).toEqual('toru')
+    })
+
+    it('will be subverted at runtime if the array element was not found', () => {
+      const sut: NNAE = new NNAE()
+
+      sut.setArrayValue(['tahi', 'rua', 'toru'])
+
+      const n1: number = 1
+      const n2: number = 1
+
+      const b: boolean = n1 == n2
+
+      expect(sut.findInArray('wha')).toBeUndefined()
+    })
+  })
+})
